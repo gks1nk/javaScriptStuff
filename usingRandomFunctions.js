8f411dc693ddc196c75658deb1f9ddb7b394ce43
gks1nk <gkresse@gmail.com>
2016-06-08 18:20:10 -0500

    Create usingRandomFunctions.js

---
+++ usingRandomFunctions.js
@@ -0,0 +1,7 @@
+//This fucntion returns a random whole number within a specified range
+function randomRange(myMin, myMax) {
+
+  return Math.floor(Math.random() * (myMax - myMin +1)) + myMin; // Change this line
+
+}
+Enter file contents here
