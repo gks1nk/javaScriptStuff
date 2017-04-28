b5bae422e7c84d4eac6ebc1cff9a4def9d53d0b3
gks1nk <gkresse@gmail.com>
2017-04-27 22:23:13 -0500

    Update intermediateJS-FCC.js

--- intermediateJS-FCC.js
+++ intermediateJS-FCC.js
@@ -137,3 +137,17 @@ function fearNotLetter(str) {
 }
 
 console.log(fearNotLetter("abcdefghjklmno"));
+
+//Boo Who
+function booWho(bool) {
+  // What is the new fad diet for ghost developers? The Boolean.
+  if (bool === true || bool === false) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+console.log(booWho([1, 2, 3]));
+
+//
