f571778b0f37d1c649009cd1c4255fbc703b5040
gks1nk <gkresse@gmail.com>
2017-06-07 18:58:05 -0500

    Update advancedJS-FCC.js

--- advancedJS-FCC.js
+++ advancedJS-FCC.js
@@ -5,3 +5,30 @@ function telephoneCheck(str) {
    return regex.test(str);
 }
 telephoneCheck("555-555-5555");
+
+//My version
+function telephoneCheck(str) {
+  // Good luck!
+  var illegalChars = /[^0-9\-\(\)\s]/g;
+  var testCase = /^(1\s?)?(\(\d{3}\)|\d{3})[\s\-]?\d{3}[\s\-]?\d{4}$/;
+  if (illegalChars.test(str)) {
+    return false;
+  } else {
+    var numDigits = str.replace(/\D/g, "").length;
+    if (numDigits > 11 || numDigits < 10) {
+      return false;
+    } else {
+      if (testCase.test(str)) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+  }
+}
+
+
+
+telephoneCheck("555-555-5555");
+
+//
